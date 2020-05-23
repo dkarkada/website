6bb057acb06654b84228356b0e0937d6c1f4a8e4
Dhruva <dkarkada@gmail.com>
2020-05-23 05:34:40 -0500

    ds course reviews

---
+++ ds-course-reviews/precache-manifest.8780e500621f1793faea2fbe9f682122.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a15d3870c2209019ca623b0cd9d3298",
+    "url": "/ds-course-reviews/index.html"
+  },
+  {
+    "revision": "439ca7408a7b0f3333d4",
+    "url": "/ds-course-reviews/static/css/main.8807dd6d.chunk.css"
+  },
+  {
+    "revision": "c237e3e00a92ad1fd740",
+    "url": "/ds-course-reviews/static/js/2.8831dcfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ds-course-reviews/static/js/2.8831dcfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439ca7408a7b0f3333d4",
+    "url": "/ds-course-reviews/static/js/main.d9823a44.chunk.js"
+  },
+  {
+    "revision": "ead5e808eb3668c50e7b",
+    "url": "/ds-course-reviews/static/js/runtime-main.2f1742ac.js"
+  },
+  {
+    "revision": "c4475155f736ee1728f535fee0709356",
+    "url": "/ds-course-reviews/static/media/tile.c4475155.png"
+  },
+  {
+    "revision": "2a3d822b68594fbfef6a12f293db8e18",
+    "url": "/ds-course-reviews/static/media/tiledark.2a3d822b.png"
+  }
+]);
